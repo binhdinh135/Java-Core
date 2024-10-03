4b79e6068b60fbebb03900448c0d4bba537fd7ed
binhdinh135 <binhdinh1351@gmai.com>
2024-10-03 09:42:33 +0700

    Boxing and Unboxing, comparing in java

--- src/CastingAndBoxing/Animal.java
+++ src/CastingAndBoxing/Animal.java
@@ -1,6 +1,25 @@
 package CastingAndBoxing;
 
 public class Animal {
+    String name;
+    int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     void makeSound() {
         System.out.println("Animal sound");
     }
